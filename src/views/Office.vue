d556d2914a4e60715ac0797a8a10e8896feb2bda
hongzhi.zhang <hongzhi.zhang@quantgroup.cn>
2026-03-23 19:03:52 +0800

    feat: 彻底去除白色背景

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -106,8 +106,8 @@ let lastMoveTime = 0
 const FRAME_INTERVAL = 500
 
 const charImages = [
-  '/character-1-fixed.png', '/character-2-fixed.png', '/character-3-fixed.png', '/character-4-fixed.png',
-  '/character-5-fixed.png', '/character-6-fixed.png', '/character-7-fixed.png', '/character-8-fixed.png'
+  '/character-1-fixed2.png', '/character-2-fixed2.png', '/character-3-fixed2.png', '/character-4-fixed2.png',
+  '/character-5-fixed2.png', '/character-6-fixed2.png', '/character-7-fixed2.png', '/character-8-fixed2.png'
 ]
 
 function initChars() {
